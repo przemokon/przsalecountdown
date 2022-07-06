b86af90ec9cf47f42ec331420ca2210bf8717bb8
przemokon <przemokon@gmail.com>
2022-07-06 12:42:34 +0200

    add polish translations

---
+++ translations/pl.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{przsalecountdown}prestashop>przsalecountdown_43f7a5f7aac38550f70edd1a33205d46'] = 'Wyświetl pozostały czas do końca promocji';
+$_MODULE['<{przsalecountdown}prestashop>przsalecountdown_73c1dfb244b29becffb541fb981e9d03'] = 'Wyświetl licznik odliczający czas do końca promocji';
+$_MODULE['<{przsalecountdown}prestashop>widget_f7e74301d4a73871707aafe2f5b2a1f6'] = 'Promocja kończy się za';
+$_MODULE['<{przsalecountdown}prestashop>widget_44fdec47036f482b68b748f9d786801b'] = 'dni';
+$_MODULE['<{przsalecountdown}prestashop>widget_73cdddd7730abfc13a55efb9f5685a3b'] = 'godzin';
+$_MODULE['<{przsalecountdown}prestashop>widget_640fd0cc0ffa0316ae087652871f4486'] = 'minut';
+$_MODULE['<{przsalecountdown}prestashop>widget_783e8e29e6a8c3e22baa58a19420eb4f'] = 'sekund';
